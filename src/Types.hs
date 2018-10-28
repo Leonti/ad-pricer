5e50d87a125a8245986a5b45b47e686e8417aa22
Leonti Bielski <leonti.bielski@myob.com>
2018-10-28 19:51:56 +1100

    Parse json config

--- src/Types.hs
+++ src/Types.hs
@@ -1,16 +1,23 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings          #-}
 module Types where
 
-import           Data.Aeson (FromJSON (..), withObject)
-import           Data.Map   (Map)
-import qualified Data.Map   as Map
+import           Data.Aeson       (FromJSON (..), FromJSONKey,
+                                   FromJSONKeyFunction (..), withObject,
+                                   withText, (.:))
+import           Data.Aeson.Types (FromJSONKey (..), Parser)
+import           Data.Map         (Map)
+import qualified Data.Map         as Map
+import           Data.Ratio       ((%))
+import           Data.Text        (Text, unpack)
 
 data Ad =
     Classic
   | Standout
   | Premium deriving (Eq, Ord, Show)
 
-newtype ThresholdQty = ThresholdQty Int deriving (Eq, Show)
-newtype DealQty = DealQty Int deriving (Eq, Show)
+newtype ThresholdQty = ThresholdQty Int deriving (Eq, Show, FromJSON)
+newtype DealQty = DealQty Int deriving (Eq, Show, FromJSON)
 
 data Deal =
     QuantityExtraAds ThresholdQty DealQty
@@ -23,10 +30,35 @@ type PricingRules = Map Ad Deal
 type CheckoutItems = Map Ad PriceQty
 data Checkout = Checkout PricingRules CheckoutItems deriving (Eq, Show)
 
-newtype Customer = Customer String deriving (Eq, Ord, Show)
+newtype Customer = Customer String deriving (Eq, Ord, Show, FromJSONKey)
 
 data Config = Config (Map Customer PricingRules) (Map Ad Rational) deriving (Eq, Show)
 
+parseAd :: Text -> Parser Ad
+parseAd x = case x of
+  "classic"  -> return Classic
+  "standout" -> return Standout
+  "premium"  -> return Premium
+  _          -> fail $ "Unknown ad type: '" <> unpack x <> "'"
+
+instance FromJSON Ad where
+  parseJSON = withText "ad" parseAd
+
+instance FromJSONKey Ad where
+  fromJSONKey = FromJSONKeyTextParser parseAd
+
+instance FromJSON Deal where
+  parseJSON = withObject "Deal" $ \o -> do
+    dealType <- o .: "type" :: Parser String
+    case dealType of
+      "qtyExtraAds"  -> QuantityExtraAds <$> o .: "threshold" <*> o .: "dealQty"
+      "qtyPriceDrop" -> QuantityPriceDrop <$> o .: "threshold" <*> ((% 100) <$> (o .: "price":: Parser Integer))
+      "priceDrop"    -> PriceDrop <$> ((% 100) <$> (o .: "price":: Parser Integer))
+      _              -> fail $ "Unknown deal type: '" <> dealType <> "'"
+
 instance FromJSON Config where
-  parseJSON = withObject "Config" $ \v ->
-    return $ Config Map.empty Map.empty
+  parseJSON = withObject "Config" $ \o -> do
+    prices <- o .: "prices" :: Parser (Map Ad Integer)
+    rules <- o .: "rules" :: Parser (Map Customer PricingRules)
+
+    return $ Config rules (Map.map (% 100) prices)
--- test/JsonConfigSpec.hs
+++ test/JsonConfigSpec.hs
@@ -3,24 +3,73 @@
 
 module JsonConfigSpec where
 
-import           Data.Aeson        (decode)
+import           Data.Aeson        (eitherDecode)
 import           Data.Map          (empty, fromList)
 import           Test.Hspec
 import           Text.RawString.QQ
 import           Types
 
 spec :: IO ()
-spec = hspec $ do
+spec = hspec $
   describe "JsonConfig" $ do
     it "should decode ad prices" $ do
       let json = [r|{
-        "prices": {
-          "classic": 26999,
-          "standout": 32299,
-          "premium": 39499
-        },
-        "rules": {}
-      }
-      |]
-      let result = decode json :: Maybe Config
-      result `shouldBe` Just (Config empty empty)
+          "prices": {
+            "classic": 10000,
+            "standout": 20000,
+            "premium": 30000
+          },
+          "rules": {}
+        }
+        |]
+      let result = eitherDecode json :: Either String Config
+      result `shouldBe` Right (Config empty (fromList [(Classic, 100),(Standout, 200),(Premium, 300)]))
+    it "should decode QuantityExtraAds rule" $ do
+      let json = [r|{
+          "prices": {},
+          "rules": {
+            "unilever": {
+              "classic": {
+                "type": "qtyExtraAds",
+                "threshold": 2,
+                "dealQty": 3
+              }
+            }
+          }
+        }
+        |]
+      let result = eitherDecode json :: Either String Config
+      result `shouldBe` Right (Config (fromList [(Customer "unilever",fromList [(Classic, QuantityExtraAds (ThresholdQty 2) (DealQty 3))])]) empty)
+
+    it "should decode QuantityPriceDrop rule" $ do
+      let json = [r|{
+          "prices": {},
+          "rules": {
+            "unilever": {
+              "classic": {
+                "type": "qtyPriceDrop",
+                "threshold": 4,
+                "price": 10000
+              }
+            }
+          }
+        }
+        |]
+      let result = eitherDecode json :: Either String Config
+      result `shouldBe` Right (Config (fromList [(Customer "unilever",fromList [(Classic, QuantityPriceDrop (ThresholdQty 4) 100)])]) empty)
+
+    it "should decode PriceDrop rule" $ do
+      let json = [r|{
+          "prices": {},
+          "rules": {
+            "unilever": {
+              "classic": {
+                "type": "priceDrop",
+                "price": 10000
+              }
+            }
+          }
+        }
+        |]
+      let result = eitherDecode json :: Either String Config
+      result `shouldBe` Right (Config (fromList [(Customer "unilever",fromList [(Classic, PriceDrop 100)])]) empty)
